7ea7c99eb8b7c3d77e2a300273856b749c43cc9a
pranavp <pranav.puranam@gmail.com>
2021-06-24 19:34:31 -0500

    completed get request for api

--- backend/api.js
+++ backend/api.js
@@ -7,23 +7,24 @@ const jsonData = JSON.parse(contents); //Contains the accessible json file
 
 //GET Procedures
 app.get('/api/procedureDropdowns', (request, response) => {
-    let procedureDict = {"procedures": []}; 
-    
+    let procedureDict = { "procedures": [] };
+
     jsonData['procedures'].forEach(element => {
-        procedureDict.procedures.push({"procedureName": element.procedureName});
+        procedureDict.procedures.push({ "procedureName": element.procedureName });
     });
-    response.send(JSON.stringify(procedureDict));
+    response.send(procedureDict);
 });
 
-app.get('/api/', (request, response) => {
-    let activitiesDict = {"activities": []};
-
-    jsonData.procedures.forEach(element => {
-        if (element.procedureName == "") {
+app.get('/api/selectDropdown', (request, response) => {
+    let activitiesDict = { "activities": [] };
+    //console.log(`The request value is ${request.query.procedureName}`);
+    jsonData['procedures'].forEach(element => {
+        if (element.procedureName === request.query.procedureName) {
             activitiesDict.activities.push(element.activities);
+            //console.log(`This is the procedure: ${element.procedureName} and the activities  ${element.activities}`);
         }
     });
-    response.send(JSON.stringify(activitiesDict));
+    response.send(activitiesDict);
 });
 
 //POST Procedures
@@ -32,3 +33,7 @@ app.post('/api/Patient', (request, response) => {
     let lastName = request.lastName;
     let visitId = request.visitId;
 });
+
+app.listen(3000, () => {
+    console.log(`Example app listening at http://localhost:${3000}`)
+})

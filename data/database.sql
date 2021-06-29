c1697b06d3b7639b934105cb4b26db469c875a97
pranavp <pranav.puranam@gmail.com>
2021-06-29 17:16:08 -0500

    changes

--- data/database.sql
+++ data/database.sql
@@ -47,3 +47,47 @@ CREATE TABLE activities (
     PRIMARY KEY (`activityid`),
     UNIQUE INDEX `activityid_UNIQUE` (`activityid` ASC) VISIBLE);
 
+--THIS IS COPY PASTED FROM WORKBENCH
+CREATE TABLE `patientsdb`.`patient` (
+  `patientid` INT NOT NULL,
+  `firstname` VARCHAR(45) NOT NULL,
+  `lastname` VARCHAR(45) NOT NULL,
+  `middlename` VARCHAR(45) NULL,
+  `gender` VARCHAR(1) NOT NULL,
+  PRIMARY KEY (`patientid`),
+  UNIQUE INDEX `patientid_UNIQUE` (`patientid` ASC) VISIBLE);
+  
+  
+CREATE TABLE `patientsdb`.`doctor` (
+  `doctorid` INT NOT NULL,
+  `firstname` VARCHAR(45) NOT NULL,
+  `lastname` VARCHAR(45) NOT NULL,
+  `association` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`doctorid`),
+  UNIQUE INDEX `doctorid_UNIQUE` (`doctorid` ASC) VISIBLE);
+
+CREATE TABLE `patientsdb`.`visit` (
+  `visitid` INT NOT NULL,
+  `admittime` DATETIME NOT NULL,
+  `dischargetime` DATETIME NOT NULL,
+  `inpatient` TINYINT NOT NULL,
+  `patientid` INT NOT NULL,
+  PRIMARY KEY (`visitid`),
+  UNIQUE INDEX `visitid_UNIQUE` (`visitid` ASC) VISIBLE);
+
+CREATE TABLE `patientsdb`.`doctor_activity` (
+  `dID` INT NOT NULL,
+  `doctorid` INT NOT NULL,
+  `visitid` INT NOT NULL,
+  `activityid` INT NOT NULL,
+  `proceduretime` DATETIME NOT NULL,
+  PRIMARY KEY (`dID`));
+
+CREATE TABLE `patientsdb`.`activities` (
+  `activityid` INT NOT NULL,
+  `type` VARCHAR(45) NOT NULL,
+  `category` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `code` INT NOT NULL,
+  PRIMARY KEY (`activityid`));
+

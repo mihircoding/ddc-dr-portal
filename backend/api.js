92ec9cef1a8b861f3dc242fc116619e576720875
pranavp <pranav.puranam@gmail.com>
2021-07-01 18:16:35 -0500

    changes

--- backend/api.js
+++ backend/api.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 const mysql = require('mysql');
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
 
 let contents = fs.readFileSync('../data/masterdata.json');
 const jsonData = JSON.parse(contents); //Contains the accessible json file
 let connection = mysql.createConnection({
     host: "localhost",
+	port: 3306,
     user: "projectuser",
-    pass: "portalservice",
+    password: "portalservice",
     database: "patientdb"
 });
 
@@ -16,33 +20,97 @@ let connection = mysql.createConnection({
 //GET Procedures
 app.get('/api/procedureDropdowns', (request, response) => {
     let procedureDict = { "procedures": [] };
+	let contents = [];
+	let strippedContents = [];
 
-    jsonData['procedures'].forEach(element => {
-        procedureDict.procedures.push({ "procedureName": element.procedureName });
-    });
-    response.send(procedureDict);
+	connection.connect(function (err) {
+		if (err) throw err;
+		
+		//Write the function in here (SELECT type, category) {procedureName : ""} 
+		//There are duplicate procedureNames
+
+		let query = "SELECT JSON_ARRAYAGG(JSON_OBJECT('procedureName', category)) FROM activities WHERE type='Procedure'";
+		connection.query(query, function(err, result) {
+			if (err) throw err;
+
+			contents = result;
+			console.log(contents);
+			strippedContents = JSON.parse(JSON.stringify(contents))[0];
+			strippedContents = JSON.parse(Object.values(strippedContents)[0]);
+			//console.log(strippedContents);
+			procedureDict.procedures = removeDuplicates(strippedContents);
+			response.send(procedureDict);
+		});
+	});
+	
 });
+
 
 app.get('/api/activities', (request, response) => {
     let activitiesDict = { "activities": [] };
-    //console.log(`The request value is ${request.query.procedureName}`);
-    jsonData['procedures'].forEach(element => {
-        if (element.procedureName === request.query.procedureName) {
-            element.activities.forEach(activity => {
-                activitiesDict.activities.push(activity)
-            })
-        }
-    });
-    response.send(activitiesDict);
+	let contents = [];
+	let strippedContents = [];
+    let category = request.query.procedureName;
+
+	let query = "SELECT JSON_ARRAYAGG(JSON_OBJECT('procedureName', category, 'activityName', name, 'code', code)) FROM activities";
+	
+	connection.query(query, function (err, result) {
+		if (err) throw err;
+
+		contents = result;
+		strippedContents = JSON.parse(JSON.stringify(contents))[0];
+		strippedContents = JSON.parse(Object.values(strippedContents)[0]);
+
+		let filtered = filter(strippedContents, category);
+		activitiesDict.activities = filtered;
+		response.send(activitiesDict);
+		
+
+		connection.end();
+		
+	});
+	
+
+    //response.send(activitiesDict);
 });
+
 
 //POST Procedures
 app.post('/api/Patient', (request, response) => {
-    let firstName = request.firstName;
-    let lastName = request.lastName;
-    let visitId = request.visitId;
+    let firstName = request.query.firstName;
+    let lastName = request.query.lastName;
+    let visitId = request.query.visitId;
 });
 
 app.listen(3000, () => {
     console.log(`Example app listening at http://localhost:${3000}`)
-})
+});
+
+//Helper Function
+function removeDuplicates(arr) {
+	let shortened = [];
+
+	arr.forEach(function (element) {
+		let unique = true;
+
+		shortened.forEach(function (element2) {
+			if (element2.procedureName == element.procedureName) unique = false;
+		});
+		if (unique) shortened.push(element);
+	});
+
+	return shortened;
+}
+
+function filter(arr, name) {
+	let stripped = [];
+
+	arr.forEach(function (element) {
+		if (element.procedureName === name) {
+			stripped.push({"activityName": element.activityName, "code": element.code});
+		}
+	});
+
+	return stripped;
+}
+
--- backend/loader.js
+++ backend/loader.js
@@ -1,11 +1,19 @@
+/*
+Create Database
+Create Tables
+Populate Tables
+*/
+
 const mysql = require('mysql');
 const fs = require('fs');
+const properties = require('./properties.js');
 
 let connection = mysql.createConnection({
-    host: "localhost:3306",
+    host: "localhost",
+	port: 3306,
     user: "projectuser",
-    pass: "portalservice",
-    database: "patientsdb",
+    password: "portalservice",
+	database: "patientdb" //Created from SQL Shell (Can add "CREATE DATABASE IF NOT EXISTS patientdb;")
 });
 
 //Connecting to the database || Create an array that will end up in the table
@@ -13,58 +21,82 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("Connection Succeeded!");
 
-    //inserting activities table
-    let template = "INSERT INTO activities (activityid, type, category, name, code) VALUES ?";
-    let data = activitiesRows();
+	let createActivities = `CREATE TABLE IF NOT EXISTS activities(
+		activityid INT PRIMARY KEY NOT NULL,
+		type VARCHAR(45) NOT NULL,
+		category VARCHAR(45) NOT NULL,
+		name VARCHAR(45) NOT NULL,
+		code INT NOT NULL
+	)`;
+	
+	connection.query(createActivities, function (err, result) {
+		if (err) {
+			console.log(err.message);
+		}
+	});
 
+
+    //inserting activities table
+    let template = "INSERT IGNORE INTO activities (activityid, type, category, name, code) VALUES ?";
+    let data = activitiesRows();
+	//console.log(data);
     connection.query(template, [data], function (err, result) {
-        if (err) throw err;
-        console.log("Num rows: " + result.affectedRows);
+       if (err) throw err;
     });
 
+
+	connection.query("SELECT * FROM activities", function (err, rows, fields) {
+		if (err) throw err;
+	
+		for (let i = 0; i < rows.length; i++) {
+			console.log(rows[i].name);
+		}
+	});
+	
     connection.end();
 });
 
 
-//Array Form - [Activity ID, Type (Procedure or Consult), Category (procuedreName), Name, code]
+//Array Form - [Activity ID, Type (Procedure or Consult), Category (procedureName), Name, code]
 //Repetitive so change this later
 function activitiesRows() {
-    let contents = fs.readFileSync('data/masterdata.json');
+    let contents = fs.readFileSync('../data/masterdata.json');
     const jsonData = JSON.parse(contents);
     let rows = []; //Nested arrays
 
     //Loop through procedures and then consults
     let temp = [];
-    let index = 1;
+	let index = 1;
     jsonData['procedures'].forEach(element => {
-        temp.push(index);
         temp.push('Procedure');
         temp.push(element.procedureName);
 
         element.activities.forEach(activity => {
-            temp = temp.slice(0, temp.length - 2);
+			temp.unshift(index);
             temp.push(activity.activityName);
             temp.push(activity.code);
             rows.push(temp);
-            index += 1;
+			temp = temp.slice(1, temp.length - 2);
+			index += 1;
         });
+		temp = [];
     });
 
     //Consults loop
     temp = [];
     jsonData['consultations'].forEach(element => {
-        temp.push(index);
         temp.push('Consultation');
         temp.push(element.consultationsName);
 
         element.activities.forEach(activity => {
-            temp = temp.slice(0, temp.length - 2);
+			temp.unshift(index);
             temp.push(activity.activityName);
             temp.push(activity.code);
             rows.push(temp);
-            index += 1;
+			temp = temp.slice(1, temp.length - 2);
+			index += 1;
         });
+		temp = [];
     });
-
     return rows; //This is a nested array that will fill an SQL table
 }
